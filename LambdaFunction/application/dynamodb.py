e62e617d4fc83a40711ddcfa22ec5bc9e32be416
Mirinano <43010381+Mirinano@users.noreply.github.com>
2022-12-10 01:40:29 +0900

    Create Class

--- LambdaFunction/application/dynamodb.py
+++ LambdaFunction/application/dynamodb.py
@@ -2,9 +2,18 @@
 import os
 import boto3
 
+TABLE_PREFIX: str = os.getenv('TABLE_PREFIX')
+
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb')
 
 class DynamoDB:
     def __init__(self, table_name: str) -> None:
-        pass
+        name = f'{TABLE_PREFIX}-{table_name}'
+        self.table = dynamodb.Table(name)
+    
+
+class SettingDynamoDB(DynamoDB):
+    def __init__(self) -> None:
+        table_name: str = "CommandSetting"
+        super().__init__(table_name)
